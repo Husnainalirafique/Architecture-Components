d673c495c3b996811bd847324c9ade0c6e4cff2d
Husnain <husnainalirafique125@gmail.com>
2023-09-23 19:40:51 +0500

    Architecture components

--- app/src/main/java/com/example/architecturecomponents/MainActivity.kt
+++ app/src/main/java/com/example/architecturecomponents/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.architecturecomponents
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.architecturecomponents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        handlingFunctions()
+
+    }
+
+    private fun handlingFunctions() {
+        handlingLateinitProperties()
+    }
+
+    private fun handlingLateinitProperties() {
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.unbind()
     }
 }
